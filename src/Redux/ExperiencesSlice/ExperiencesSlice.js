140b5cefd78bd5517bc50243139dce147bb983ee
HFG43 <hernanguemes@hotmail.com>
2023-10-31 19:00:13 -0300

    Feat: add expierences reducer

--- src/Redux/ExperiencesSlice/ExperiencesSlice.js
+++ src/Redux/ExperiencesSlice/ExperiencesSlice.js
@@ -4,10 +4,24 @@ import axios from 'axios';
 const getExperiencesUrl = 'https://jsonplaceholder.typicode.com/posts';
 const postExperienceUrl = 'xxxx';
 
-const initialstate = {
-  experiencesData: [],
+const initialState = {
+  experiences: [],
   status: 'idle',
   error: null,
+};
+
+const experiencesFetched = (experiences) => {
+  const experiencesList = experiences.map((experience) => ({
+    experience_id: experience.id,
+    experience_name: experience.name,
+    experience_photo: experience.photo,
+    experience_description: experience.description,
+    experience_fee: experience.experience_fee,
+    experience_testing_fee: experience.add_testing_fee,
+    experience_reserve_full_table: experience.reserve_full_table,
+    experience_guests: experience.guests,
+  }));
+  return experiencesList;
 };
 
 export const getExperiencesData = createAsyncThunk(
@@ -35,4 +49,28 @@ export const addNewExperience = createAsyncThunk(
   },
 );
 
-export const teste = 'testeo';
+const experiencesSlice = createSlice({
+  name: 'experiences',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getExperiencesData.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getExperiencesData.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.experiencesData = action.payload;
+      })
+      .addCase(getExperiencesData.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+      .addCase(addNewExperience.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default experiencesSlice.reducer;
