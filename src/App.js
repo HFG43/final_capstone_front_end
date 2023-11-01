0c5b792323c718b5a9b8fb5162f1a126cabf07e2
Dachrono <andres_zamorano_@hotmail.com>
2023-11-01 08:22:38 -0600

    Add the default route for login page, Add width for the main container

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import LoginPage from './components/LoginPage';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Routes">
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/MainPage" element={<MainPage />} />
+      </Routes>
     </div>
   );
 }
