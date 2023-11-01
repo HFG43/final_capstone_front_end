30960ad9b24a1c011efaaad76d3c606bce541bb6
Dachrono <andres_zamorano_@hotmail.com>
2023-11-01 08:25:14 -0600

    Add user reducer

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { logger } from 'redux-logger';
 // import experiencesReducer from './Experienceslice/ExperiencesSlice';
 // import reservationsReducer from './ReservationsSlice/ReservationsSlice';
+import usersSliceReducer from './slices/userSlice';
 
 const store = configureStore({
   reducer: {
     // experiences: experiencesReducer,
     // reservations: reservationsReducer,
+    users: usersSliceReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
