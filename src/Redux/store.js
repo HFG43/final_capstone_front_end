e45f4ea8c32143b34972ce9ecf43be84994fb773
Carmen Bravo <nbravoalcala@gmail.com>
2023-11-02 17:40:19 -0400

    Add the reservationsReducer in the redux store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,13 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { logger } from 'redux-logger';
 import usersReducer from './Slices/usersSlice';
 import experiencesReducer from './Slices/ExperiencesSlice';
-// import reservationsReducer from './ReservationsSlice/ReservationsSlice';
+import reservationsReducer from './Slices/reservationsSlice';
 
 const store = configureStore({
   reducer: {
     users: usersReducer,
     experiences: experiencesReducer,
-    // reservations: reservationsReducer,
+    reservations: reservationsReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
