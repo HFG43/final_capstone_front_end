d296f6bd6b914aadf5cb64f90df34bc9e2e8eabf
Andrea Manuel <andreamanuel2207@gmail.com>
2023-11-02 18:16:27 -0600

    Delete routes that belongs to another task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import './Style/register.css';
+import './Style/medias.css';
 import { getExperiencesData } from './Redux/Slices/ExperiencesSlice';
-import LoginPage from './Components/LoginPage';
-import MainPage from './Components/MainPage';
 
 function App() {
   const dispatch = useDispatch();
@@ -17,12 +16,7 @@ function App() {
   }, [dispatch, status]);
 
   return (
-    <div className="Routes">
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/MainPage" element={<MainPage />} />
-      </Routes>
-    </div>
+    <div className="Routes" />
   );
 }
 
