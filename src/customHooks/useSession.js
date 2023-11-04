530b6ddd6844bad9f2e4c411db256904a61369b4
Andrea Manuel <andreamanuel2207@gmail.com>
2023-11-03 18:04:34 -0600

    Change the name from customHooks directory to logic

--- src/customHooks/useSession.js
+++
@@ -1,8 +0,0 @@
-import { useSelector } from 'react-redux';
-
-function useSession() {
-  const user = useSelector((state) => state.users.user);
-  return user?.id && { user };
-}
-
-export default useSession;
