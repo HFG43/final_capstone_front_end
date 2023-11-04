bfbed6dda201a023441efe4230f4dbde07d3128a
Andrea Manuel <andreamanuel2207@gmail.com>
2023-11-04 17:59:44 -0600

    Fetch the user reservations

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 import './Style/register.css';
 import './Style/medias.css';
+import './Style/myReservation.css';
 import { getExperiencesData } from './Redux/Slices/ExperiencesSlice';
 import { loadUserFromLocalStorage } from './Redux/Slices/usersSlice';
+import { getUserReservations } from './Redux/Slices/reservationsSlice';
 
 function App() {
   const dispatch = useDispatch();
   const status = useSelector((state) => state.experiences.status);
+  const { id } = useSelector((state) => state.users.user);
 
   useEffect(() => {
     if (status === 'idle') {
-      dispatch(getExperiencesData());
       // Set user infromation from localStorage
       dispatch(loadUserFromLocalStorage());
+      dispatch(getExperiencesData());
     }
   }, [dispatch, status]);
+
+  useEffect(() => {
+    if (status === 'succeeded') {
+      dispatch(getUserReservations(id));
+    }
+  });
 
   return (
     <div className="Routes" />
