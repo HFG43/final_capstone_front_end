00eaec982b86f0b20c3d2457bd1197c0909fb827
Dachrono <andres_zamorano_@hotmail.com>
2023-11-06 15:51:55 -0600

    Create swiper carrousel for all experiences in the database

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,14 +1,35 @@
+import { Link } from 'react-router-dom';
+import { register } from 'swiper/element/bundle';
 import { useSelector } from 'react-redux';
 import React from 'react';
 import '../Style/Main.css';
+import 'swiper/css';
+
+register();
 
 function MainPage() {
   const DataUser = useSelector((state) => state.users);
+  const DataExperiences = useSelector((state) => state.experiences);
 
   return (
     <div className="mainContainer">
       <h1>{`Welcome ${DataUser.user.name}`}</h1>
       <p className="secondtitle">at the best experiences in the world</p>
+      <swiper-container slides-per-view="3" navigation="true">
+        {DataExperiences.experiences.map((item) => (
+          <swiper-slide key={item.id}>
+            <Link className="cardlink" to={`./Register/${item.id}`}>
+              <div className="card">
+                <h2>{item.name}</h2>
+                <img src={item.image} alt="fotito" />
+                <p>Experience description:</p>
+                <p className="cardDesc">{item.description}</p>
+
+              </div>
+            </Link>
+          </swiper-slide>
+        ))}
+      </swiper-container>
     </div>
   );
 }
