9e5438265883bb1e2261982425e4db9c221964f9
Carmen Bravo <nbravoalcala@gmail.com>
2023-11-09 20:04:52 -0400

    Update the placeholder of the Experience image field - AddExperience form

--- src/Components/AddExperience.jsx
+++ src/Components/AddExperience.jsx
@@ -60,7 +60,7 @@ const AddExperience = () => {
             <input id="testing" type="number" placeholder="Additional Testing Fee" className="form-input reserv-input" required min="0.01" step="0.01" />
             <input id="fulltable" type="number" placeholder="Additional Full Table Fee" className="form-input reserv-input" required min="0.01" step="0.01" />
             <input id="guests" type="number" placeholder="Guests included in Experience" className="form-input reserv-input" required min="1" step="1" />
-            <input id="image" type="text" placeholder="Add an Experience image" className="form-input reserv-input" required />
+            <input id="image" type="text" placeholder="Add Experience image URL" className="form-input reserv-input" required />
           </div>
           <input type="submit" className="form-submit register_but_exp" onClick={(e) => CreateExperience(e)} value="Register" />
         </form>
